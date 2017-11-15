7639c9bdb3437dc4d051c576c1b7df0db3cbe98a
Roger Guldbrandsen <roger@kinbiko.com>
2017-11-15 20:07:12 +0000

    Initial spring

---
+++ java/src/main/java/org/bathml/parking/ParkingApplication.java
@@ -0,0 +1,12 @@
+package org.bathml.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApplication.class, args);
+	}
+}
